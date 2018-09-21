b418a34a993dd70c4cc14c984aded4b5df244980
Vasiliy Uskov <vasiliy.uskov@ispringsolution.com>
2018-09-21 16:52:48 +0300

    Доработал Observable. Добавил возможность задавать приоритета observer`ам

--- lab2/src/Observer.ts
+++ lab2/src/Observer.ts
@@ -5,22 +5,31 @@ interface IObserver<T> {
 }
 
 abstract class Observable<T> {
-    public addObserver(observer: IObserver<T>) {
-        this._observers.set(observer, true);
+    public addObserver(observer: IObserver<T>, priority: number = this._greaterPriority) {
+        this._greaterPriority = priority > this._greaterPriority ? priority : this._greaterPriority;
+        this._observers.set(observer, {
+            active: true,
+            priority,
+        });
     }
 
     public removeObserver(observer: IObserver<T>) {
-        this._observers.set(observer, false);
+        this._observers.get(observer).active = false;
         this._observersRemoveList.push(observer);
     }
 
     public notifyObservers() {
-        const observers = this._observers.keys();
-        for (const observer of observers) {
-            if (this._observers.get(observer)) {
+        const sortedObservers = this._observers.keys().slice();
+        sortedObservers.sort((observer1: IObserver<T>, observer2: IObserver<T>) => {
+            const priority1 = this._observers.get(observer1).priority;
+            const priority2 = this._observers.get(observer2).priority;
+            return priority1 > priority2 ? 1 : (priority1 == priority2 ? 0 : -1);
+        });
+        for (const observer of sortedObservers) {
+            if (this._observers.get(observer).active) {
                 observer.update(this._getObservableData());
             }
-        };
+        }
         this._clearRemoveList();
     }
 
@@ -33,8 +42,9 @@ abstract class Observable<T> {
         this._observersRemoveList = [];
     }
 
-    private _observers: TSMap<IObserver<T>, boolean> = new TSMap();
+    private _observers: TSMap<IObserver<T>, {active: boolean, priority: number}> = new TSMap();
     private _observersRemoveList: Array<IObserver<T>> = [];
+    private _greaterPriority: number = 0;
 }
 
 export {IObserver};
