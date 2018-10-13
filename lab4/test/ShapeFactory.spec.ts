6e753b032bc036fa2205f9b8a5fd8cd948ff638a
Vasiliy <vasus714@yandex.ru>
2018-10-13 20:01:11 +0300

    LAB_4 Добавил тесты для ShapeFactory

---
+++ lab4/test/ShapeFactory.spec.ts
@@ -0,0 +1,128 @@
+import { assert } from "chai";
+import {expect} from "~chai/lib/Chai";
+import {ShapeFactory} from "../src/ShapeFactory";
+import {Rectangle} from "../src/shape/Rectangle";
+import {Vec2} from "../src/Vec2";
+import {Color} from "../src/Color";
+import {Triangle} from "../src/shape/Triangle";
+import {Polygon} from "../src/shape/Polygon";
+import {Ellipse} from "../src/shape/Ellipse";
+
+const shapeFactory = new ShapeFactory();
+const invalidShapeTypeError = new Error("Incorrect shape description");
+const invalidInputTypeError = new Error("Incorrect input type");
+
+describe("ShapeFactory", () => {
+    describe("should throw exception", () => {
+        it("when shape type is invalid", () => {
+            try {
+                const rectangle = shapeFactory.createShape({
+                    type: "other shape"
+                });
+            }
+            catch (e) {
+                expect(e).equal(invalidShapeTypeError);
+            }
+        });
+        it("when color is invalid", () => {
+            try {
+                const rectangle = shapeFactory.createShape({
+                    type: "rectangle",
+                    vertex1: {x: 1, y: 1},
+                    vertex2: {x: 2, y: 2}
+                });
+            }
+            catch (e) {
+                expect(e).equal(invalidInputTypeError);
+            }
+        });
+        it("when try to create rectangle without vertexes", () => {
+            try {
+                const rectangle = shapeFactory.createShape({
+                    type: "rectangle",
+                    color: 10,
+                });
+            }
+            catch (e) {
+                expect(e).equal(invalidInputTypeError);
+            }
+        });
+        it("when try to create triangle without vertexes", () => {
+            try {
+                const rectangle = shapeFactory.createShape({
+                    type: "rectangle",
+                    color: 10,
+                });
+            }
+            catch (e) {
+                expect(e).equal(invalidInputTypeError);
+            }
+        });
+        it("when try to create ellipse with string radius", () => {
+            try {
+                const rectangle = shapeFactory.createShape({
+                    type: "polygon",
+                    color: Color.Red,
+                    center: {x: 0, y: 1},
+                    radiusX: "0",
+                    radiusY: "1",
+                });
+            }
+            catch (e) {
+                expect(e).equal(invalidInputTypeError);
+            }
+        });
+        it("when try to create ellipse without center", () => {
+            try {
+                const rectangle = shapeFactory.createShape({
+                    type: "polygon",
+                    color: Color.Red,
+                    radiusX: "0",
+                    radiusY: "1",
+                });
+            }
+            catch (e) {
+                expect(e).equal(invalidInputTypeError);
+            }
+        });
+    });
+    describe("should create by correct data", () => {
+        it("rectangle", () => {
+            const rectangle = shapeFactory.createShape({
+                type: "rectangle",
+                color: Color.Red,
+                vertex1: {x: 1, y: 1},
+                vertex2: {x: 2, y: 2}
+            });
+            expect(rectangle).equal(new Rectangle(Color.Red, new Vec2(1, 1), new Vec2(2, 2)));
+        });
+        it("triangle", () => {
+            const rectangle = shapeFactory.createShape({
+                type: "triangle",
+                color: Color.Red,
+                vertex1: {x: 0, y: 1},
+                vertex2: {x: 1, y: 0},
+                vertex3: {x: 0, y: 0}
+            });
+            expect(rectangle).equal(new Triangle(Color.Red, new Vec2(0, 1), new Vec2(1, 0), new Vec2(0, 0)));
+        });
+        it("polygon", () => {
+            const rectangle = shapeFactory.createShape({
+                type: "polygon",
+                color: Color.Red,
+                vertexes: [{x: 0, y: 1}],
+            });
+            expect(rectangle).equal(new Polygon(Color.Red, [new Vec2(0, 1)]));
+        });
+        it("ellipse", () => {
+            const rectangle = shapeFactory.createShape({
+                type: "polygon",
+                color: Color.Red,
+                center: {x: 0, y: 1},
+                radiusX: 0,
+                radiusY: 1,
+            });
+            expect(rectangle).equal(new Ellipse(Color.Red, new Vec2(0, 1), 0, 1));
+        })
+    });
+});
