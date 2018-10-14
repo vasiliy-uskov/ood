a33fd840589653403e4eef23727a339363116f84
Vasiliy <vasus714@yandex.ru>
2018-10-14 23:29:01 +0300

    LW4 Доработал Canvas, который может рисовать на svg

--- lab4/src/app.ts
+++ lab4/src/app.ts
@@ -10,34 +10,32 @@ painter.drawPicture(designer.createDraft([
     {
         type: "ellipse",
         color: 0,
-        center: {x: 1, y: 1},
-        radiusX: 1,
-        radiusY: 1
+        center: {x: 20, y: 20},
+        radiusX: 10,
+        radiusY: 10
     },
     {
         type: "triangle",
         color: 1,
-        vertex1: {x: 0, y: 1},
-        vertex2: {x: 1, y: 0},
+        vertex1: {x: 0, y: 20},
+        vertex2: {x: 20, y: 0},
         vertex3: {x: 0, y: 0},
     },
     {
         type: "rectangle",
         color: 1,
-        vertex1: {x: 1, y: 1},
-        vertex2: {x: 2, y: 2},
+        vertex1: {x: 0, y: 0},
+        vertex2: {x: 20, y: 20},
     },
     {
         type: "polygon",
         color: 1,
         vertexes: [
-            {x: 0, y: 1},
-            {x: 2, y: 3},
-            {x: 4, y: 5},
-            {x: 6, y: 7},
-            {x: 8, y: 9},
-            {x: 9, y: 8},
-            {x: 7, y: 6}
+            {x: 0, y: 10},
+            {x: 10, y: 10},
+            {x: 10, y: 20},
+            {x: 20, y: 20},
+            {x: 20, y: 0},
         ]
     }
 ]), canvas);
