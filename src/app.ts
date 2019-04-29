5e074277a987c6dbba0f15428ac6a2aaca7cc2dd
Vasiliy Uskov <vasiliy.uskov@ispringsolution.com>
2019-04-29 15:22:36 +0300

    LAB_3_task-3 Провёл рефакторинг точки входа

--- src/app.ts
+++ src/app.ts
@@ -6,31 +6,35 @@ import {simpleTsezarDecrypt, simpleTsezarEncrypt, TranslateInputStream} from "./
 import {CompressInputStream} from "./CompressInputStream";
 import {DecompressInputStream} from "./DecompressInputStream";
 
+function getStreamDecoratorByKey(key: string, getArgument: () => string): (arg: IInputStream) => IInputStream {
+	if (key == "--encrypt") {
+		const key = parseInt(getArgument());
+		if (!isNaN(key)) {
+			return (stream) => new TranslateInputStream(stream, simpleTsezarEncrypt(key));
+		}
+	}
+	if (key == "--decrypt") {
+		const key = parseInt(getArgument());
+		if (!isNaN(key)) {
+			return (stream) => new TranslateInputStream(stream, simpleTsezarDecrypt(key));
+		}
+	}
+	if (key == "--compress") {
+		return (stream) => new CompressInputStream(stream);
+	}
+	if (key == "--decompress") {
+		return (stream) => new DecompressInputStream(stream);
+	}
+	return stream => stream
+}
 
 function main(argv: Array<string>): void {
-	argv = argv.slice(2);
+	argv = argv.slice(2); // delete node.exe and scrypt name from arguments array
+
 	const outputStream = new FileOutputStream(argv.pop());
 	let inputStream: IInputStream = new FileInputStream(argv.pop());
 	while (argv.length) {
-		const option = argv.shift();
-		if (option == "--encrypt") {
-			const key = parseInt(argv.shift());
-			if (!isNaN(key)) {
-				inputStream = new TranslateInputStream(inputStream, simpleTsezarEncrypt(key));
-			}
-		}
-		if (option == "--decrypt") {
-			const key = parseInt(argv.shift());
-			if (!isNaN(key)) {
-				inputStream = new TranslateInputStream(inputStream, simpleTsezarDecrypt(key));
-			}
-		}
-		if (option == "--compress") {
-			inputStream = new CompressInputStream(inputStream);
-		}
-		if (option == "--decompress") {
-			inputStream = new DecompressInputStream(inputStream);
-		}
+		inputStream = getStreamDecoratorByKey(argv.shift(), () => argv.shift())(inputStream);
 	}
 	const chunkSize = 100;
 	while (!inputStream.eof()) {
