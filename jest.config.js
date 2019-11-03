411ef95ec7ee6b2278a0a056b960403aa7c6ed0d
Vasiliy Uskov <vasiliy.uskov@ispringsolution.com>
2019-11-04 02:12:18 +0300

    LAB_5 Доработал тесты.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  coveragePathIgnorePatterns: ["/node_modules/", "/tests/"],
+  coveragePathIgnorePatterns: [
+      "/node_modules/",
+      "/tests/",
+      "/src/model/element", // structures typedefs
+      "src/model/Document.ts" // document typedefs
+  ],
   collectCoverage: true,
+  collectCoverageFrom: [
+      "./src/**/*.ts"
+  ]
 };
--- tests/commands/HelpCommand.spec.ts
+++ tests/commands/HelpCommand.spec.ts
@@ -1,42 +1,12 @@
 import {createEditor} from "../mocks/DocumentData";
 import {HelpCommand} from "../../src/command/HelpCommand";
-import {DeleteItemCommand} from "../../src/command/DeleteItemCommand";
-import {InsertImageCommand} from "../../src/command/InsertImageCommand";
-import {InsertParagraphCommand} from "../../src/command/InsertParagraphCommand";
-import {ReplaceParagraphTextCommand} from "../../src/command/ReplaceParagraphTextCommand";
-import {ResizeImageCommand} from "../../src/command/ResizeImageCommand";
-import {SaveDocumentCommand} from "../../src/command/SaveDocumentCommand";
-import {UndoCommand} from "../../src/command/UndoCommand";
-import {RedoCommand} from "../../src/command/RedoCommand";
-import {ListCommand} from "../../src/command/ListCommand";
-import {SetDocumentTitleCommand} from "../../src/command/SetDocumentTitleCommand";
-import {ICommand} from "../../src/command/ICommand";
-import {ExitCommand} from "../../src/command/ExitCommand";
 import {ConsoleLogMock} from "../mocks/ConsoleLogMock";
+import {createCommands} from "../../src/command/CommandsCreator";
 
 it('return help', () => {
 	const command = new HelpCommand([]);
 	expect(command.help()).toMatchSnapshot();
 });
-
-function createAllCommands(): Array<ICommand> {
-	const editor = createEditor();
-	const commands: Array<ICommand> = [
-		new DeleteItemCommand(editor),
-		new InsertImageCommand(editor),
-		new InsertParagraphCommand(editor),
-		new ReplaceParagraphTextCommand(editor),
-		new ResizeImageCommand(editor),
-		new SaveDocumentCommand(editor, () => {}),
-		new UndoCommand(editor),
-		new RedoCommand(editor),
-		new ExitCommand(),
-		new ListCommand(editor, () => {}),
-		new SetDocumentTitleCommand(editor),
-	];
-	commands.push(new HelpCommand(commands));
-	return commands;
-}
 
 describe('is', () => {
 	it('return true if command start by DeleteItem', () => {
@@ -49,7 +19,7 @@ describe('is', () => {
 });
 describe('execute', () => {
 	it('log to console help', () => {
-		const command = new HelpCommand(createAllCommands());
+		const command = new HelpCommand(createCommands(createEditor()));
 		ConsoleLogMock.startMock();
 		command.execute();
 		expect(ConsoleLogMock.result()).toMatchSnapshot();
